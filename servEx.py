cf71b1a54a7d979e67fdf5649560b6a4a62a27bf
Boris Fesenko <fesenko@mail.ru>
2021-07-31 12:24:19 +0300

    Update servEx.py

--- servEx.py
+++ servEx.py
@@ -7,27 +7,27 @@ def server_program():  # именная функция
     # на котором в настоящее время выполняется интерпретатор Python
     host = socket.gethostname()
     print(host)
-    port = 5000  # инициируем номер порта (5000)
-    server_socket = socket.socket()  # создание сокета
-    server_socket.bind((host, port))  # связываем сокет с хостом и портом
-    # запуск режима прослушиания кол-во клиентов, которых облужиает сервер одновременно - 2 шт.
-    server_socket.listen(1)
-    # принять подключение, возвращает кортеж с двумя элементами: новый сокет и адрес клиента
-    conn, address = server_socket.accept()  # вот здесь оно останавлиается
+    # port = 5000  # инициируем номер порта (5000)
+    # server_socket = socket.socket()  # создание сокета
+    # server_socket.bind((host, port))  # связываем сокет с хостом и портом
+    # # запуск режима прослушиания кол-во клиентов, которых облужиает сервер одновременно - 2 шт.
+    # server_socket.listen(1)
+    # # принять подключение, возвращает кортеж с двумя элементами: новый сокет и адрес клиента
+    # conn, address = server_socket.accept()  # вот здесь оно останавлиается
 
-    # Вывод о соединении
-    print("Connection from: " + str(address))
+    # # Вывод о соединении
+    # print("Connection from: " + str(address))
 
-    while True:  # бесконечный цикл пока не возникнет break
-        # контроль объема пакетов получения данных recv(1024) не больше 1024 байта
-        # decode() - возвращает строку, декодированную из заданных байтов
-        data = conn.recv(1024).decode()
-        if not data:  # если данные не получены break
-            break
-        print("from connected user: " + str(data))
-        data = input(' -> ')
-        conn.send(data.encode())  # отправить данные клиенту
-    conn.close()  # закрыть соединение
+    # while True:  # бесконечный цикл пока не возникнет break
+    #     # контроль объема пакетов получения данных recv(1024) не больше 1024 байта
+    #     # decode() - возвращает строку, декодированную из заданных байтов
+    #     data = conn.recv(1024).decode()
+    #     if not data:  # если данные не получены break
+    #         break
+    #     print("from connected user: " + str(data))
+    #     data = input(' -> ')
+    #     conn.send(data.encode())  # отправить данные клиенту
+    # conn.close()  # закрыть соединение
 
 
 if __name__ == '__main__':
